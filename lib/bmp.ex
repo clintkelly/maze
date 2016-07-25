7d05e828099383b471dd3c62ef89beee42de8ee7
Clint Kelly <clint.kelly@airbnb.com>
2016-07-24 18:25:43 -0700

    Add optional method to change cell fill

--- lib/bmp.ex
+++ lib/bmp.ex
@@ -6,7 +6,7 @@ defmodule MazeWalls.Bmp do
 
   alias MazeWalls.Grid
 
-  def as_bmp(grid, cell_size \\ 10) do
+  def as_bmp(grid, cell_size \\ 10, cell_color \\ fn(_loc) -> Color.named(:black) end) do
 
     # Add 1 to height and width for walls on south and east
     canvas = Canvas.size(%Size{height: grid.nrows*cell_size+1, width: grid.ncols*cell_size+1}) 
@@ -15,18 +15,32 @@ defmodule MazeWalls.Bmp do
     canvas = Enum.reduce(
       Grid.get_locations(grid),
       canvas,
-      fn cell, canvas -> with_cell(canvas, cell, grid) end)
+      fn cell, canvas -> with_cell(canvas, cell, grid, cell_size, cell_color) end)
 
     Bump.write(filename: "foo.bmp", canvas: canvas)
 
   end
 
-  def with_cell(canvas, cell={row,col}, grid, cell_size \\ 10) do
+  def with_cell(canvas, cell={_row,_col}, grid, cell_size \\ 10, cell_color \\ fn(_loc) -> Color.named(:black) end) do
     canvas
     |> maybe_with_wall_to_north(cell, grid, cell_size)
     |> maybe_with_wall_to_west(cell, grid, cell_size)
     |> maybe_with_wall_to_east(cell, grid, cell_size)
     |> maybe_with_wall_to_south(cell, grid, cell_size)
+    |> with_cell_fill(cell, grid, cell_size, cell_color)
+  end
+
+  def with_cell_fill(canvas, cell={row,col}, grid, cell_size, cell_color) do
+      x1 = col * cell_size + 1
+      y1 = (grid.nrows - (row+1)) * cell_size + 1
+      Canvas.fill(
+        canvas, 
+        color: cell_color.(cell),
+        rect: %Rect{ 
+          origin: %Point{ x: x1, y: y1 }, 
+          size: %Size{ height: cell_size-1, width: cell_size-1 },
+        }
+      )
   end
 
   def maybe_with_wall_to_east(canvas, cell={row,col}, grid, cell_size \\ 10) do
